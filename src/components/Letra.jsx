8d1d5f8d55f96e489ed83abe34e794f94fdc756d
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-05-15 21:12:09 -0500

    Agregando proyecto busqueda de letras de canciones

---
+++ src/components/Letra.jsx
@@ -0,0 +1,13 @@
+import Spinner from "./Spinner"
+import useLetras from "../hooks/useLetras"
+
+const Letra = () => {
+  const { letra, cargando } = useLetras()
+
+  return (
+    cargando ? <Spinner/> :
+    <div className="letra">{letra}</div>
+  )
+}
+
+export default Letra
